739e7a1c4fcf7f47eb5683b2c92ed8c28ec02ad7
Nidhi357 <nkeshri@maximaconsulting.com>
2016-05-17 19:31:42 +0530

    Added services in project file.

--- src/main/webapp/modules/project/controllers.js
+++ src/main/webapp/modules/project/controllers.js
@@ -1,11 +1,15 @@
 'use strict';
 angular.module('Project').controller('ProjectController', function($scope, ProjectService) {
-	$scope.id = 1;
-	ProjectService.list($scope.id,function(response) {
+/*	$scope.id = 1;*/
+
+	ProjectService.list(function(response) {
+
         if(response.success) {
         	alert(JSON.stringify(response));
         } else {
         	alert(JSON.stringify(response));
         }
     });
+	
 });
+
--- src/main/webapp/modules/project/services.js
+++ src/main/webapp/modules/project/services.js
@@ -25,9 +25,37 @@ angular.module('Project').service('ProjectService',function($http) {
 		this.serviceCaller("POST", '/project/get', id, callback);
 	};
 	
-	this.list = function(id, callback) {
+
+	this.list = function(callback) {
 		this.serviceCaller("POST", '/project/list', "", callback);
 	};
+	
+	this.add=function(project,callback){
+		this.serviceCaller("POST", '/project/add', "", callback);
+	};
+	
+	this.update=function(existingObj,callback){
+		this.serviceCaller("POST", '/project/update', "", callback);
+	};
+	
+	this.deletee=function(id,callback){
+		this.serviceCaller("POST", '/project/delete', "", callback);
+	};
+	
+	
+	this.getAll=function(ids,callback){
+		this.serviceCaller("POST", '/project/getAll', "", callback);
+	};
+	
+	this.search=function(json,callback){
+		this.serviceCaller("POST", '/project/search', "", callback);
+	};
+		
+	this.deleteAll=function(ids,callback){
+		this.serviceCaller("POST", '/project/deleteAll', "", callback);
+	};
+
+
 
 });
 
