80824738786e8915ddc72e467e619c3b12b71fc6
Nidhi357 <nkeshri@maximaconsulting.com>
2016-05-20 12:15:53 +0530

    project.html has been created

--- src/main/webapp/modules/project/controllers.js
+++ src/main/webapp/modules/project/controllers.js
@@ -1,8 +1,19 @@
 'use strict';
 angular.module('Project').controller('ProjectController', function($scope, ProjectService) {
+	 $scope.months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+	 $scope.closeAlert=function(index){
+		$scope.months.splice(index,1); 
+		alert("Deleted");
+	 };
+	 
+	 $scope.edit=function($event){
+		alert("Edited");
+	 };
+	 
+	 
 	ProjectService.list(function(response) {
-
         if(response.success) {
+        	 $scope.months=response;
         	alert(JSON.stringify(response));
         } else {
         	alert(JSON.stringify(response));
